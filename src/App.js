b997c1191bf7a00aa9c52a0c9514a0fdb43bc776
Sianah Small <smallsianah@gmail.com>
2025-01-31 21:36:04 -0800

    included video, but kept commented out. Need to re-install git lfs potentially

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import img from './img/CBC Logo.png';
+// import video from './img/CBC.mp4';
+import site1 from './img/site1.png';
+import site2 from './img/site2.png';
+import site3 from './img/site3.png';
+import React from 'react';
+import ReactPlayer from 'react-player';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import Balancer from 'react-wrap-balancer';
+
+
+// function vidPlay(){
+//   return(
+//     <ReactPlayer 
+//     url={video} 
+//     playing = {true}
+//     loop = {true}
+//     muted = {true}
+//     width = "100%" 
+//     height = "100%" 
+//      />
+//   );
+// }
 
 function App() {
   return (
@@ -36,8 +59,64 @@ function App() {
           </div>
         </div>
       </nav>
+      {/* {vidPlay()} */}
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', marginTop: '20px' }}>
+        <FontAwesomeIcon icon={faAngleDown} size="2x" color='#CB9B2B' style={{ marginBottom: '-20px' }}/>
+        <FontAwesomeIcon icon={faAngleDown} size="2x" color='#CB9B2B'/>
+      </div>
+      <div className="sites" style={{display:'flex'}}>
+        <img src={site1} alt="Site1"/>
+        <p style={{marginLeft:'700px', marginTop:'50px', textAlign:'justify', flex: 1, flexWrap:'wrap', textWrap:'balance'}}>
+          <Balancer>Rorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, 
+          dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, 
+          ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus.
+          Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
+          Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. 
+          Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. 
+          Aliquam in elementum tellus.</Balancer>
+          </p>
+      </div>
+      <div className="sites2" style={{display:'flex'}}>
+        <p style={{marginLeft:'100px', marginTop:'50px', textAlign:'justify', flex: 1, flexWrap:'wrap', textWrap:'balance'}}>
+          <Balancer>Rorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, 
+          dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, 
+          ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus.
+          Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
+          Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. 
+          Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. 
+          Aliquam in elementum tellus.</Balancer>
+          </p>
+          <img src={site2} alt="Site1"/>
+      </div>
+      <div className="sites" style={{display:'flex'}}>
+        <img src={site3} alt="Site1"/>
+        <p style={{marginLeft:'700px', marginTop:'50px', textAlign:'justify', flex: 1, flexWrap:'wrap', textWrap:'balance'}}>
+          <Balancer>Rorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, 
+          dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, 
+          ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus.
+          Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
+          Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. 
+          Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. 
+          Aliquam in elementum tellus.</Balancer>
+          </p>
+      </div>
+      <footer className="footer">
+        <div className = "footer-titles" style={{display:'flex' , justifyContent:'space-between'}}>
+        <h3 style={{color:'#333'}}>Construction By Cadena</h3>
+        <h3 style={{color:'#333'}}>Socials</h3>
+        <h3 style={{color:'#333'}}>inquiries</h3>
+        </div>
+        <h4 style={{color: '#878787'}}>Lorem ipsum dolor sit amet, consectetur <br/>adipiscing elit.</h4>
+        <ul/>
+        <h4 style={{color: '#878787'}}>123 Sesame Street</h4>
+        <ul/>
+        <h4 style={{color: '#878787'}}>Office: (123)-456-7890<br/>
+        Support@ConstructionByCadena.com</h4>
+      </footer>
     </div>
   );
 }
 
+
 export default App;
+<script src="https://kit.fontawesome.com/aea2883ddf.js" crossorigin="anonymous"></script>
